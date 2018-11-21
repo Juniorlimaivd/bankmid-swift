59d43370826fe2774c42e716911e25329aca47fe
Unknown <junior.lima.ivd.2@gmail.com>
2018-11-20 21:44:59 -0300

    Adding login screen

---
+++ bankmidtest/bankmidtest/LoginViewController.swift
@@ -0,0 +1,54 @@
+//
+//  LoginViewController.swift
+//  bankmidtest
+//
+//  Created by Junior Lima on 20/11/18.
+//  Copyright © 2018 Junior Lima. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func ditTappedToLogin(_ sender: Any) {
+        if accountTextField.text == "" || passwordTextField.text == "" {
+            let alert = UIAlertController(title: "Error", message: "Por favor preencha o usuário e senha antes de logar.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true)
+            return
+        }
+        
+        let proxy = BankProxy(username: accountTextField.text!, password: passwordTextField.text!)
+        let balance = proxy.getBalance(accountTextField.text!)
+        
+        if balance == 0.0 {
+            let alert = UIAlertController(title: "Error", message: "Credenciais inválidas.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            
+            self.present(alert, animated: true)
+            
+            self.accountTextField.text = ""
+            self.passwordTextField.text = ""
+        }
+    }
+    
+
+}
